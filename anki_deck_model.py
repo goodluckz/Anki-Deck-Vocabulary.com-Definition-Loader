5dec1c739b67bf842ddd79351ea51a6a904203d8
Niekler, Christian <christian.niekler@student.fhws.de>
2021-08-01 18:46:45 +0200

    introduce another model

--- anki_deck_model.py
+++ anki_deck_model.py
@@ -16,3 +16,28 @@ def get_card_model():
               'afmt': '{{FrontSide}}<hr id="answer">{{definition}}<br><br>{{Example}}',
             },
           ])
+          
+          
+def get_card_model_2():
+   return genanki.Model(
+        1607392319,
+        'Simple Model',
+        fields=[
+            {'name': 'Word'},
+            {'name': 'Definition'},
+            {'name': 'Synonym'},
+            {'name': 'Antonym'},
+            {'name': 'Sentence'},
+            {'name': 'Part of Speech'},
+            {'name': 'Transcript'},
+            {'name': 'Source'},
+            {'name': 'Picture'},
+            {'name': 'Audio'}
+          ],
+          templates=[
+            {
+              'name': 'Card 1',
+              'qfmt': '{{Term}}',
+              'afmt': '{{FrontSide}}<hr id="answer">{{definition}}<br><br>{{Example}}',
+            },
+          ])
