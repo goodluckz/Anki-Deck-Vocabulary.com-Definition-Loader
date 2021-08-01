84e8224b709d44424bc6ceab395398ac0377e3dc
Niekler, Christian <christian.niekler@student.fhws.de>
2021-08-01 19:19:15 +0200

    add HTML for deck model 2

--- anki_deck_model.py
+++ anki_deck_model.py
@@ -38,7 +38,39 @@ def get_card_model_2():
         templates=[
             {
                 'name': 'Card 1',
-                'qfmt': '{{Term}}',
-                'afmt': '{{FrontSide}}<hr id="answer">{{definition}}<br><br>{{Example}}',
+                'qfmt': """
+                <div class="header">
+                <span class="word">{{Word}}</span>
+                </div>
+                
+                <div class="header">
+                <span class="transcription">{{Transcription}}</span>
+                <span class="part-of-speech">{{Part of Speech}}</span>
+                </div>
+                <span class="audio">{{Audio}}</span>""",
+
+                'afmt': """
+                {{FrontSide}}
+                
+                <hr id="answer">
+                
+                <div class="sub-header">
+                <span class="definition">{{Definition}}</span>
+                </div>
+                
+                {{#Synonym}}
+                <div class="sub-header">
+                <span class="synonym"><b>Synonym</b> {{Synonym}}</span>
+                </div>
+                {{/Synonym}}
+                {{#Antonym}}
+                <div class="sub-header">
+                <span class="antonym"><b>Antonym:</b> {{Antonym}}</span>
+                </div>
+                
+                {{/Antonym}}
+                <div class="sentence">{{Sentence}}</div>
+                <div class='source'>{{Source}}</div>
+                <div class="picture">{{Picture}}</div>""",
             },
         ])
